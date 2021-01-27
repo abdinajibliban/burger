a15ac867aae1de781212a819386287f4c5cfb42f
Abdinajib Liban <abdinajibliban@gmail.com>
2021-01-27 00:52:10 -0500

    changed connection.js

--- config/connection.js
+++ config/connection.js
@@ -13,12 +13,6 @@ if (process.env.JAWSDB_URL) {
   });
 };
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected ");
-});
+connection.connect();
 
 module.exports = connection;
